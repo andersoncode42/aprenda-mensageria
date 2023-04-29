e75264f68f90e31e564bbd7fbb6a4048a0c56438
Anderson <andersonsergipano@gmail.com>
2023-04-29 11:26:42 -0300

    Mudando o nome da lib de meu-projeto para aprenda-mensageria

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="meu-projeto",
-    version="0.1.0",
-    description="Um exemplo de pacote python",
+    name="aprenda-mensageria",
+    version="0.0.1",
+    description="Uma tentativa de pacote python para testar mensageria",
     packages=["model"],
-    package_dir={"": "src"}, # Indica que os pacotes estão na pasta src
+    # Indica que os pacotes estão na pasta src
+    package_dir={"": "src"},
     install_requires=[],
 )
