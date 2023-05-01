87b2c42d5dda95dae7765a4510e37b06373f1af8
Anderson <andersonsergipano@gmail.com>
2023-05-01 16:56:01 -0300

    Refatoração

--- src/mensageria/conector.py
+++ src/mensageria/conector.py
@@ -1,14 +1,11 @@
-# pip install pika
-# pip install logging
-
 from enum import Enum
+from logador import Logador
 import pika
 from pika.exceptions import AMQPError
-from logador import Logador
 
 
 class Conector:
-    """Classe que contém provê informação/recursos para se conectar à mensageria"""
+    """Classe que provê informações / recursos para se conectar à mensageria"""
 
     # Credencias para se conectar
     MENSAGERIA_HOST = "localhost"
@@ -19,43 +16,50 @@ class Conector:
     EXCHANGE_NOME = "roteador"
     EXCHANGE_TIPO = "topic"
 
-    # Dados das filas associadas À exchange
     class Fila(Enum):
         """Filas e suas respectivas routing keys"""
-        PENDENTES = "p"
-        CONCLUIDOS = "c"
-        ERROS = "e"
+        PENDENTES = "PENDENTES"
+        CONCLUIDOS = "CONCLUIDOS"
+        ERROS = "ERROS"
 
     def getchannel(self):
         """Cria uma conexão com a mensageria e retorna um objeto dot tipo Canal Bloqueante
             Returns:
                 (BlockingChannel): Um canal do RabbitMQ
         """
-        credenciais = pika.PlainCredentials(self.MENSAGERIA_USUARIO, self.MENSAGERIA_SENHA)
 
-        conexao = pika.BlockingConnection(
-            pika.ConnectionParameters(host=self.MENSAGERIA_HOST, credentials=credenciais)
-        )
+        try:
+            credenciais = pika.PlainCredentials(self.MENSAGERIA_USUARIO, self.MENSAGERIA_SENHA)
 
-        canal = conexao.channel()
+            conexao = pika.BlockingConnection(
+                pika.ConnectionParameters(host=self.MENSAGERIA_HOST, credentials=credenciais)
+            )
 
-        # Declarar um exchange do tipo topic chamado "roteador"
-        canal.exchange_declare(exchange=self.EXCHANGE_NOME,
-                               exchange_type=self.EXCHANGE_TIPO)
+            canal = conexao.channel()
 
-        return canal
+            # Declarar um exchange do tipo topic chamado "roteador"
+            canal.exchange_declare(exchange=self.EXCHANGE_NOME,
+                                   exchange_type=self.EXCHANGE_TIPO)
 
-    def publique(self, msg, rota):
-        """Método responsável por publicar a mensagem na Exchange"""
+            return canal
+        except Exception as excecao:
+            Logador.error("Erro inesperado ao criar canal", excecao)
+
+    def publique(self, msg, fila):
+        """Publica a mensagem na fila
+            Parameters:
+                msg (str): A mensagem a ser publicada
+                fila (str): O nome da fila
+        """
 
         canal = self.getchannel()
 
         try:
-            print(f"Tentando enviar a mensagem: {msg}, para a rota: {rota}")
+            print(f"Tentando enviar a mensagem: {msg}, para a rota: {fila}")
 
             # Publicar uma mensagem no exchange com a routing key correspondente ao status
-            canal.basic_publish(exchange="roteador",
-                                routing_key=rota,
+            canal.basic_publish(exchange=self.EXCHANGE_NOME,
+                                routing_key=fila,
                                 body=msg)
 
             # Registrar uma mensagem de sucesso
@@ -69,39 +73,23 @@ class Conector:
             # Registrar uma mensagem de erro em caso de qualquer outra exceção
             Logador.error("Erro inesperado ao enviar mensagem", excecao)
         finally:
+            # Lembre-se de liberar / fechar o recurso
             canal.close()
 
-    def __init__(self):
-        """Construtor da classe que recebe as propriedades como parâmetros
-
+    def consome(self, fila):
+        """Retorna a mansagem da fila
             Parameters:
-                idlote (int): O id do lote de processos
-                idcategoria (int): O id que representa a categoria dos processos
-                lista_de_processos (list): Uma lista de processos
+                fila (str): O nome da fila
+            Returns:
+                (str): A mensagem da fila
         """
-        # Atribua os valores dos parâmetros às propriedades da classe
-        pass
 
-    @staticmethod
-    def insira_pendente(self, lote_de_processos):
-        """Insere na fila de Pendentes
-
-        """
-        # TODO document why this method is empty
-        pass
-
-    @staticmethod
-    def insira_concluido(self, lote_de_processos):
-        """Insere na fila de Concluídos
-
-                """
-        # TODO document why this method is empty
-        pass
-
-    @staticmethod
-    def insira_erro(self, lote_de_processos):
-        """Insere na fila de Erros
-
-        """
-        # TODO document why this method is empty
-        pass
+        canal = self.getchannel()
+        try:
+            metodo, propriedades, msg = canal.basic_get(queue=fila, no_ack=True)
+            return msg
+        except Exception as excecao:
+            Logador.error(f"Erro inesperado ao consumir mensagem da fila: {fila}", excecao)
+        finally:
+            # Sempre se lembre de liberar / fechar o recurso
+            canal.close()
