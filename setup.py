9cf941205c052ef58dfa9696599c9b3a9cddba4a
Anderson <andersonsergipano@gmail.com>
2023-05-01 23:07:32 -0300

    Adicionado o pacote mensageria na definição 'packages'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="aprenda-mensageria",
     version="0.0.1",
     description="Uma tentativa de pacote python para testar mensageria",
-    packages=["model"],
+    packages=["model", "mensageria"],
     # Indica que os pacotes estão na pasta src
     package_dir={"": "src"},
     install_requires=[],
