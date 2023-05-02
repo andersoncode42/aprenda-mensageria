3e6ab5ea9cb9ee71f10d7a07ef65a375a1263512
Anderson <andersonsergipano@gmail.com>
2023-05-01 23:31:33 -0300

    .

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+# OLD
+"""
 setup(
     name="aprenda-mensageria",
     version="0.0.1",
@@ -8,4 +10,14 @@ setup(
     # Indica que os pacotes estão na pasta src
     package_dir={"": "src"},
     install_requires=[],
+)
+"""
+
+setup(
+    name="aprenda-mensageria",
+    version="0.0.1",
+    description="Uma tentativa de pacote python para testar mensageria",
+    packages=find_packages('src'),
+    package_dir={"": "src"},
+    install_requires=["pika~=1.3.1"],
 )
